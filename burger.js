ccdbd20ca520d6c1f93c2037063f64950a4ebbc5
Nuralidev <nuralimammadovis@gmail.com>
2024-11-11 00:00:11 +0400

    v2

--- burger.js
+++ burger.js
@@ -1,20 +1,22 @@
 const { Bot } = require("grammy");
 
 // Create a bot object
-const bot = new Bot(""); // <-- place your bot token in this string
-
+const bot = new Bot("7918774506:AAE9V_19-_QZo_9bD1XRS5zGt4R6g-NTALM"); // <-- place your bot token in this string
+let orders={}
 // Register listeners to handle messages
 bot.on("message:text", (ctx) => {
     const chatId=ctx.message.chat.id
-    const message=ctx.message.text.trim();
+    let message=ctx.message.text.trim();
     if(message==="/start"){
         ctx.reply(`Salam ${ctx.message.chat.first_name}\n Sifaris Nomresini bize gonderin ve sifarisiniz hazir olduqda bu barede size bildiris gonderilecek.`)
     }
-    if (!isNaN(message)) { // Əgər nömrədirsə
+    message=parseInt(message)
+    if (!isNaN(message) && typeof(message)==typeof(5)) { // Əgər nömrədirsə
         orders[message] = { chatId: chatId, status: 'Gözləyir' };
-        ctx.reply(chatId, `Sifariş nömrəniz (${message}) qəbul edildi. Hazır olduqda bildiriş alacaqsınız.`);
+        console.log(orders)
+        ctx.reply(`Sifariş nömrəniz ${message} qəbul edildi. Hazır olduqda bildiriş alacaqsınız.`);
     } else {
-        ctx.reply(chatId, 'Xahiş edirik, yalnız sifariş nömrənizi göndərin.');
+        ctx.reply('Xahiş edirik, yalnız sifariş nömrənizi göndərin.');
     }
 });
 
